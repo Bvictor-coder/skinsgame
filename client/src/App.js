b6a4cce46542eb92925c479dbdf7495288dfc5b5
Bvictor-coder <bavictor@calpoly.edu>
2025-03-31 12:13:59 -0700

    Fix useNavigate import ESLint warning

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { BrowserRouter as Router, Routes, Route, useNavigate, Navigate } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import { UserProvider, useUser, ROLES } from './utils/userContext';
 import Header from './components/Header';
 import PlayersManagement from './components/PlayersManagement';
