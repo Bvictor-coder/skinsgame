ca929858c27c03b526a1e3f442650e727962b2d8
Bvictor-coder <bavictor@calpoly.edu>
2025-03-31 10:27:31 -0700

    Fix Netlify build error: Add missing useCallback import to PairingsAndGroups component

--- client/src/components/PairingsAndGroups.js
+++ client/src/components/PairingsAndGroups.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import dataSync from '../utils/dataSync';
 
 const PairingsAndGroups = () => {
